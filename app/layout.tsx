ff8863baa24521ab8f91cfb54a1b82ca9fc54b88
ceky <cekyy07@gmail.com>
2024-11-13 19:06:03 +0200

    Fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/NavBar";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Find a Mountain Guide",
   description: "Connect with Professional Mountain Guides",
 };
 
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen max-w-6xl mx-auto`}>
