f3ef326614cd542f70f71fca4d326a1e994743c6
ceky <cekyy07@gmail.com>
2024-12-06 11:41:35 +0200

    Fix env file

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_API_BASE_URL || "http://localhost:8080",
+  baseURL:
+    process.env.NEXT_PUBLIC_REACT_API_BASE_URL || "http://localhost:8080",
   headers: {
     post: {
       "Content-Type": "application/json",
